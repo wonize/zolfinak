e9a63824739c810419ad8bb89487ba0cbd90bdfc
mikoloism <mikoloism@gmail.com>
2024-01-26 01:00:34 +0330

    refactor(pages): rename `Tab2` page to `Daily` only

--- src/pages/DailyPage.tsx
+++ src/pages/DailyPage.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './DailyPage.css';
 
 const Tab2: React.FC = () => {
   return (
