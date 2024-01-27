e827937398af217a8b87a77f9432a3def616b4ae
mikoloism <mikoloism@gmail.com>
2024-01-27 19:15:36 +0330

    feat(i18n): wrap `IonApp` children with customized `I18nextProvider`

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { IonApp, IonIcon, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { albumsOutline, appsOutline, settingsOutline } from 'ionicons/icons';
+import { AppRouter } from './App.router';
+import { I18nextProvider } from './features/i18n/mod';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -19,31 +21,32 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 
 /* Theme variables */
-import { AppRouter } from './App.router';
 import './theme/variables.css';
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <AppRouter />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="home" href="/home">
-            <IonIcon aria-hidden="true" icon={appsOutline} />
-          </IonTabButton>
-          <IonTabButton tab="daily" href="/daily">
-            <IonIcon aria-hidden="true" icon={albumsOutline} />
-          </IonTabButton>
-          <IonTabButton tab="settings" href="/settings">
-            <IonIcon aria-hidden="true" icon={settingsOutline} />
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+    <I18nextProvider>
+      <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <AppRouter />
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="home" href="/home">
+              <IonIcon aria-hidden="true" icon={appsOutline} />
+            </IonTabButton>
+            <IonTabButton tab="daily" href="/daily">
+              <IonIcon aria-hidden="true" icon={albumsOutline} />
+            </IonTabButton>
+            <IonTabButton tab="settings" href="/settings">
+              <IonIcon aria-hidden="true" icon={settingsOutline} />
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonReactRouter>
+    </I18nextProvider>
   </IonApp>
 );
 
