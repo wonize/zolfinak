3f4637f64768036ff7ec21e0727dce5b82cc4e1e
mikoloism <mikoloism@gmail.com>
2024-01-28 15:05:46 +0330

    feat(i18n): add scopes as enum type
    - scope isn't have enum-like behavior, but by this new type its can be work like it.
    - update default scope with new Scope Enum and Scope Object

--- src/features/i18n/token.ts
+++ src/features/i18n/token.ts
@@ -23,8 +23,10 @@ export type I18nScope = Readonly<{
   [Scope in Uppercase<keyof I18nToken>]: Lowercase<Scope>;
 }>;
 
+export type I18nScopeEnum = I18nScope[keyof I18nScope];
+
 export const I18nNSList = Object.keys(I18nScope).map((ns) =>
   String(ns).toLowerCase(),
 ) as ReadonlyArray<`${(typeof I18nScope)[keyof typeof I18nScope]}`>;
 
-export const DEFAULT_I18N_NAMESPACE = 'app' as const;
+export const DEFAULT_I18N_NAMESPACE = I18nScope.APP;
