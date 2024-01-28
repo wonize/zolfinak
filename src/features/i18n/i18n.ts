f2d9a6358ecf42d254894d95c4527cdaef10ec06
mikoloism <mikoloism@gmail.com>
2024-01-28 08:37:47 +0330

    feat(i18n): support namespaces (ns or scope)
    - add namespaces to `i18next.init` as `ns` property
    - add `defaultNS` to `18next.init`
    - replace type of `resrouces` inside declared module to `LocaleMap` type

--- src/features/i18n/i18n.ts
+++ src/features/i18n/i18n.ts
@@ -2,16 +2,19 @@ import i18n from 'i18next';
 import { createElement, type PropsWithChildren } from 'react';
 import { initReactI18next, I18nextProvider as RealI18nextProvider } from 'react-i18next';
 import { DEFAULT_LANG_CODE, getLastLanguageCode, LangCode, LangName } from './code';
-import { LocaleMap as resources } from './locales';
+import { LocaleMap as resources, type LocaleMap } from './locales';
+import { DEFAULT_I18N_NAMESPACE, I18nNSList } from './token';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    resrources: typeof resources;
+    resrources: LocaleMap;
   }
 }
 
 i18n.use(initReactI18next).init({
   resources,
+  ns: I18nNSList,
+  defaultNS: DEFAULT_I18N_NAMESPACE,
   lng: getLastLanguageCode(),
   fallbackLng: DEFAULT_LANG_CODE,
   supportedLngs: [LangCode[LangName.ENGLISH], LangCode[LangName.PERSIAN]],
