23e13902a3339b24d0c9b2e4732172fe1e0160d1
mikoloism <mikoloism@gmail.com>
2024-01-28 08:28:53 +0330

    perf(i18n): improve Token and Scope
    - revert to `type` from `interface` keyword
    	related: 919b21010a75008f0531a770034df7a153ff4779
    - support custom `scope` (in i18next call `namespace` or `ns`)
    	- add `app` scope as default scope instead of `translation`
    	- revert `home` and `settings` scrope
    - add scope enum as `I18nScope`
    - add default scope constant
    - add available scopes as `Array` to pass to `i18next.init` into `ns`
    	property
    - support types of things in semi-automate way

--- src/features/i18n/token.ts
+++ src/features/i18n/token.ts
@@ -1,12 +1,30 @@
-export interface Token {
-  home_hero_title: string;
-  home_inline_search_placeholder: string;
-  home_hubspoke_title: string;
-  settings_title: string;
-  settings_about_link_text: string;
-  settings_language_label: string;
-}
-
 export type I18nToken = {
-  translation: Token;
+  home: {
+    hero_title: string;
+    inline_search_placeholder: string;
+    hubspoke_title: string;
+  };
+  settings: {
+    title: string;
+    about_link_text: string;
+    language_label: string;
+  };
+  app: {};
 };
+
+/** i18next namespaces(ns) */
+export const I18nScope: I18nScope = {
+  HOME: 'home',
+  SETTINGS: 'settings',
+  APP: 'app',
+} as const;
+
+export type I18nScope = Readonly<{
+  [Scope in Uppercase<keyof I18nToken>]: Lowercase<Scope>;
+}>;
+
+export const I18nNSList = Object.keys(I18nScope).map((ns) =>
+  String(ns).toLowerCase(),
+) as ReadonlyArray<`${(typeof I18nScope)[keyof typeof I18nScope]}`>;
+
+export const DEFAULT_I18N_NAMESPACE = 'app' as const;
