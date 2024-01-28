c73b834d144933ebec57e998f9df4062c0f84c98
mikoloism <mikoloism@gmail.com>
2024-01-28 08:34:04 +0330

    perf(i18n): improve Locales Map
    - remove dynamic import of lacole json files
    - add static import of local json files
    	- related: 375029d0055b56d87cf5cabd23b8326df4605bd8
    - support type-safe `LocaleMap`(in `i18next` call `resrouces`)

--- src/features/i18n/locales.ts
+++ src/features/i18n/locales.ts
@@ -1,16 +1,22 @@
 import { LangCode } from './code';
-import type { I18nToken, Token } from './token';
+import type { I18nToken } from './token';
 
-async function load(code: LangCode): Promise<I18nToken> {
-  const translation: Token = await import(`../../locales/${code}.json`);
-  return { translation };
-}
+// locales
+import AR from '../../locales/ar.json';
+import EN from '../../locales/en.json';
+import FA from '../../locales/fa.json';
+import RU from '../../locales/ru.json';
+import TR from '../../locales/tr.json';
+import ZH from '../../locales/zh.json';
+// locales end
 
-export const LocaleMap = {
-  [LangCode.ARABIC]: await load(LangCode.ARABIC),
-  [LangCode.CHINESE]: await load(LangCode.CHINESE),
-  [LangCode.ENGLISH]: await load(LangCode.ENGLISH),
-  [LangCode.PERSIAN]: await load(LangCode.PERSIAN),
-  [LangCode.RUSSIAN]: await load(LangCode.RUSSIAN),
-  [LangCode.TURKISH]: await load(LangCode.TURKISH),
+export const LocaleMap: LocaleMap = {
+  [LangCode.ARABIC]: AR,
+  [LangCode.ENGLISH]: EN,
+  [LangCode.PERSIAN]: FA,
+  [LangCode.RUSSIAN]: RU,
+  [LangCode.TURKISH]: TR,
+  [LangCode.CHINESE]: ZH,
 } as const;
+
+export type LocaleMap = Readonly<{ [lang in LangCode]: I18nToken }>;
