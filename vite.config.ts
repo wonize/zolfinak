6493840399bae2805c9e2743ae7632896e6c7f69
mikoloism <mikoloism@gmail.com>
2024-02-03 21:48:19 +0330

    chore(config): reference `vitest/config` by 3slash
    - in `vite.config.ts` error displayed:
    
    No overload matches this call.
    The last overload gave the following error.
    Object literal may only specify known properties, and 'test' does not exist in type 'UserConfigExport'.
    
    - to fix, added `///` (three-slash) declaration
    	to reference type defination
    	of `vitest/config`
    	in `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+/// <reference types="vitest/config" />
+
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-  }
-})
+  },
+});
