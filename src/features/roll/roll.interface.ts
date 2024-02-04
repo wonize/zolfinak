7b4f9e7cf59c3456d1df47346a8307011bf8ae23
mikoloism <mikoloism@gmail.com>
2024-02-04 13:04:57 +0330

    feat(roll): add `HighComponent` type to `roll.interface.ts` file
    a generic type to annotation higher-order component
    	type with generic `P` as `props` type

--- src/features/roll/roll.interface.ts
+++ src/features/roll/roll.interface.ts
@@ -6,9 +6,15 @@ export type RollOut<In extends object> =
   | React.ComponentType<In>
   | Iterable<React.ReactElement>;
 
-export type Roll<In extends object, Out = RollOut<In>> = (Component: React.ComponentType<In>) => (props: In) => Out;
+export type Roll<In extends object, Out = RollOut<In>> =
+  | ((Component: React.ComponentType<In>) => (props: In) => Out)
+  | HighComponent<In>;
 
 export interface Rollable<In extends object, Out> {
   roll<T extends object = In, P extends object = In & T>(rolled: Roll<P, Out>): Rollable<P, Out>;
   around<T>(Component: React.ComponentType<In & T>): React.ComponentType<Out>;
 }
+
+export type HighComponent<P extends object, C extends React.ComponentType<P> = React.ComponentType<P>> = (
+  component: C & React.ComponentType<P>,
+) => (props: P) => React.ReactNode | React.ReactElement | JSX.Element | React.ComponentType;
