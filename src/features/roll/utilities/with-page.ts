a6ce9780e5219ac92bf97c151639fd9a22b3f015
mikoloism <mikoloism@gmail.com>
2024-02-04 13:05:41 +0330

    refactor(roll): add `HightComponent` to `withPage` HOC

--- src/features/roll/utilities/with-page.ts
+++ src/features/roll/utilities/with-page.ts
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import type { HighComponent } from '../roll.interface';
 
-export function withPage(path: string, option: WithPageOption = DEFAULT_WITH_PAGE_OPTION) {
-  return function HigherOrderComponent<P>(component: React.ComponentType<P>) {
+export function withPage<P extends object>(
+  path: string,
+  option: WithPageOption = DEFAULT_WITH_PAGE_OPTION,
+): HighComponent<P> {
+  return function HigherOrderComponent(component: React.ComponentType<P>) {
     return function EnhancedComponent(props: P): JSX.Element {
       return React.createElement(Route, Object.assign({}, { path, component }, option, props));
     };
