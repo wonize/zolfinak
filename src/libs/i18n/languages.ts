e936bc91a5bae318b16065a49e2a6dadf82b1a45
mikoloism <mikoloism@gmail.com>
2024-02-08 17:08:21 +0330

    fix(i18n): remove `LangBuilder`
    - in `languages.ts` file
    - removed `LangBuilder`
    - removed `LangDirector`
    
    - change position of static lang enums
    	By move languages from start of `LanguageName`
    	to end of `LanguageName`.
    	Because, To add the languages when cosntructor
    	invoked into `LangaugeName.langs` object map.
    
    - make static lang enums with `LanguageName` itself
    	instead of `LangBuilder` and `LangDirector`
    	Because, The `LanguageName` cannot be used
    	before initialized in `LangBuilder`.

--- src/libs/i18n/languages.ts
+++ src/libs/i18n/languages.ts
@@ -3,91 +3,21 @@ export const enum TextDirection {
   LTR = 'ltr',
 }
 
-class LangBuilder<T extends string> {
-  public constructor() {}
-
-  #key!: T;
-  public withKey(key: T): this {
-    this.#key = key;
-    return this;
-  }
-
-  #code!: string;
-  public withCode(code: string): this {
-    this.#code = code;
-    return this;
-  }
-
-  #native!: string;
-  public withNative(native: string): this {
-    this.#native = native;
-    return this;
-  }
-
-  #dir: TextDirection = TextDirection.LTR;
-  public withDirection(dir: TextDirection): this {
-    this.#dir = dir;
-    return this;
-  }
-
-  public make(): LanguageName<T> {
-    return new LanguageName<T>(this.#key, this.#native, this.#code, this.#dir);
-  }
-
-  public static new<T extends string>(): LangBuilder<T> {
-    return new LangBuilder<T>();
-  }
-}
-
-class LangDirector {
-  public static makeArabic(): LanguageName<'ARABIC'> {
-    return new LangBuilder<'ARABIC'>().withKey('ARABIC').withNative('العربية').withCode('ar').make();
-  }
-
-  public static makeChinese(): LanguageName<'CHINESE'> {
-    return new LangBuilder<'CHINESE'>().withKey('CHINESE').withNative('中文（简体字）').withCode('zh').make();
-  }
-
-  public static makeEnglish(): LanguageName<'ENGLISH'> {
-    return new LangBuilder<'ENGLISH'>().withKey('ENGLISH').withNative('English').withCode('en').make();
-  }
-
-  public static makePersian(): LanguageName<'PERSIAN'> {
-    return new LangBuilder<'PERSIAN'>().withKey('PERSIAN').withNative('پارسی').withCode('fa').make();
-  }
-
-  public static makeRussian(): LanguageName<'RUSSIAN'> {
-    return new LangBuilder<'RUSSIAN'>().withKey('RUSSIAN').withNative('Русский').withCode('ru').make();
-  }
-
-  public static makeTurkish(): LanguageName<'TURKISH'> {
-    return new LangBuilder<'TURKISH'>().withKey('TURKISH').withNative('Türkçe').withCode('tr').make();
-  }
-}
-
 export class LanguageName<T extends string = LanguageNameKey> {
-  public static readonly ARABIC: LanguageName<'ARABIC'> = LangDirector.makeArabic();
-  public static readonly CHINESE: LanguageName<'CHINESE'> = LangDirector.makeChinese();
-  public static readonly ENGLISH: LanguageName<'ENGLISH'> = LangDirector.makeEnglish();
-  public static readonly PERSIAN: LanguageName<'PERSIAN'> = LangDirector.makePersian();
-  public static readonly FARSI: LanguageName<'PERSIAN'> = LanguageName.PERSIAN;
-  public static readonly RUSSIAN: LanguageName<'RUSSIAN'> = LangDirector.makeRussian();
-  public static readonly TURKISH: LanguageName<'TURKISH'> = LangDirector.makeTurkish();
-
   private static langs: Record<string, LanguageNameKey> = {};
-  public static readonly findKey = (target: string): LanguageNameKey => {
+  public static findKey(target: string): LanguageNameKey {
     return LanguageName.langs[target as keyof typeof LanguageName.langs];
-  };
-  public static readonly get = <T extends LanguageNameKey = LanguageNameKey>(target: string): LanguageName => {
-    const key: T = LanguageName.findKey(target) as T;
-    return Object(LanguageName)[key] as LanguageName<T>;
-  };
+  }
+  public static get(target: string): LanguageName {
+    const key = LanguageName.findKey(target);
+    return Object(LanguageName)[key];
+  }
 
   public constructor(
     protected readonly _value: T,
     protected readonly _native: string,
     protected readonly _code: string,
-    protected readonly _dir: TextDirection,
+    protected readonly _dir: TextDirection = TextDirection.LTR,
   ) {
     LanguageName.langs = Object.assign({}, LanguageName.langs, {
       [_value]: _value,
@@ -124,6 +54,28 @@ export class LanguageName<T extends string = LanguageNameKey> {
   public [Symbol.toStringTag](): string {
     return this.toString();
   }
+
+  public static readonly ARABIC: LanguageName<'ARABIC'> = new LanguageName<'ARABIC'>(
+    'ARABIC',
+    'العربية',
+    'ar',
+    TextDirection.RTL,
+  );
+  public static readonly CHINESE: LanguageName<'CHINESE'> = new LanguageName<'CHINESE'>(
+    'CHINESE',
+    '中文（简体字）',
+    'zh',
+  );
+  public static readonly ENGLISH: LanguageName<'ENGLISH'> = new LanguageName<'ENGLISH'>('ENGLISH', 'English', 'en');
+  public static readonly PERSIAN: LanguageName<'PERSIAN'> = new LanguageName<'PERSIAN'>(
+    'PERSIAN',
+    'پارسی',
+    'fa',
+    TextDirection.RTL,
+  );
+  public static readonly FARSI: LanguageName<'PERSIAN'> = LanguageName.PERSIAN;
+  public static readonly RUSSIAN: LanguageName<'RUSSIAN'> = new LanguageName<'RUSSIAN'>('RUSSIAN', 'Русский', 'ru');
+  public static readonly TURKISH: LanguageName<'TURKISH'> = new LanguageName<'TURKISH'>('TURKISH', 'Türkçe', 'tr');
 }
 
 export type LanguageNameKey = Exclude<keyof typeof LanguageName, 'prototype' | 'findKey' | 'find' | 'get'>;
